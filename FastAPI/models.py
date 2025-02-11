ee51e161937ecf8e8d5cb15128c8829c9ad28cd1
heagenb03 <heagenbell@outlook.com>
2025-02-10 20:26:50 -0600

    add buyin db model

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -2,10 +2,10 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, Float
 
 class BuyIn(Base):
-    __tablename__ = 'buyins'
+    __tablename__ = "buy_in"
 
     id = Column(Integer, primary_key=True, index=True)
-    player = Column(String)
-    amount = Column(Float)
-    is_active = Column(Boolean, default=True)
-    
+    name = Column(String)
+    buy_in_price = Column(Float)
+    cash_out_price = Column(Float)
+    winnings_losings_price = Column(Float)
