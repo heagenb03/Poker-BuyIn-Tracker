ff504ad883fe5f8d28a53fb487f655f345bef316
heagenb03 <heagenbell@outlook.com>
2025-02-10 20:26:11 -0600

    add sqlite db connection and session

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-URL = 'sqlite:///./test.db'
-engine = create_engine(URL, connect_args={"check_same_thread": False})
+URL_DB = "sqlite:///./test.db"
+
+engine = create_engine(URL_DB, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
+Base = declarative_base()
